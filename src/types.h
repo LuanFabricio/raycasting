b981e86358f7f78a1c548df05d4bcbc69ae7063a
LuanFabricio <luanlimaleite@hotmail.com>
2024-10-19 23:19:00 -0300

    feat: Adding f32, f64, vec2f32 and vec2u32 types

--- src/types.h
+++ src/types.h
@@ -1,8 +1,18 @@
 #ifndef __SRC_TYPES_H__
 #define __SRC_TYPES_H__
 
+typedef float f32;
+typedef double f64;
 typedef unsigned int u32;
 typedef int i32;
+
+typedef struct {
+	f32 x, y;
+} vec2f32_t;
+
+typedef struct {
+	u32 x, y;
+} vec2u32_t;
 
 typedef struct {
 	u32 width, height;
