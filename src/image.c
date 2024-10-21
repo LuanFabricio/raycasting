4f59f14752b2035cb6cbbdb7a5c5a48582772300
LuanFabricio <luanlimaleite@hotmail.com>
2024-10-20 22:14:39 -0300

    feat: Adding image_clear function

--- src/image.c
+++ src/image.c
@@ -27,6 +27,12 @@ image_t image_create(u32 width, u32 height, u32* pixel_buffer)
 	};
 }
 
+void image_clear(image_t *img, u32 color)
+{
+	const u32 img_len = img->width * img->height;
+	for (u32 i = 0; i < img_len; i++) img->pixel_buffer[i] = color;
+}
+
 void image_set_pixel(image_t *img, u32 x, u32 y, u32 color)
 {
 	img->pixel_buffer[xy_to_index(x, y, img->width)] = color;
@@ -55,8 +61,8 @@ void image_append_image(image_t* target, const image_t* src, vec2u32_t position)
 	u32 max_y = src->height-1;
 	if (position.y + max_y >= target->height) max_y = target->height - position.y - 1;
 
-	for (u32 x = 0; x <= max_x; x++) {
-		for (u32 y = 0; y <= max_y; y++) {
+	for (u32 y = 0; y <= max_y; y++) {
+		for (u32 x = 0; x <= max_x; x++) {
 			const u32 target_index = xy_to_index(
 					position.x+x, position.y+y, target->width);
 			const u32 target_color = target->pixel_buffer[target_index];
@@ -65,7 +71,6 @@ void image_append_image(image_t* target, const image_t* src, vec2u32_t position)
 			const u32 src_color = src->pixel_buffer[src_index];
 
 		   	target->pixel_buffer[target_index] = blend_colors(src_color, target_color);
-
 		}
 	}
 }
