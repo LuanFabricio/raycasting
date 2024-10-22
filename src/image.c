fd5a072beb15d2962bf16a301b3d58f97a784152
LuanFabricio <luanlimaleite@hotmail.com>
2024-10-22 00:11:39 -0300

    feat: Moving blend_colors and xy_to_index to utils.h

--- src/image.c
+++ src/image.c
@@ -1,22 +1,5 @@
 #include "image.h"
-
-const u32 blend_colors(const u32 src, const u32 target)
-{
-	const f32 src_alpha = (f32)(src & 0xff) / 0xff;
-	const f32 src_alpha_c = 1.0f - src_alpha;
-
-	// TODO: Review this
-	const u32 final_color = (u32)((target & (0xff << 8 * 3)) * src_alpha_c + (src & (0xff << 8 * 3)) * src_alpha) // Red
-		| (u32)((target & (0xff << 8 * 2)) * src_alpha_c + (src & (0xff << 8 * 2)) * src_alpha) // Green
-		| (u32)((target & (0xff << 8 * 1)) * src_alpha_c + (src & (0xff << 8 * 1)) * src_alpha) // Blue
-		| 0xff; // Alpha
-	return final_color;
-}
-
-const u32 xy_to_index(const u32 x, const u32 y, const u32 width)
-{
-	return width * y + x;
-}
+#include "utils.h"
 
 image_t image_create(u32 width, u32 height, u32* pixel_buffer)
 {
---
+++ src/utils.h
@@ -0,0 +1,24 @@
+#ifndef __SRC_UTILS_H__
+#define __SRC_UTILS_H__
+
+#include "types.h"
+
+static const u32 blend_colors(const u32 src, const u32 target)
+{
+	const f32 src_alpha = (f32)(src & 0xff) / 0xff;
+	const f32 src_alpha_c = 1.0f - src_alpha;
+
+	// TODO: Review this
+	const u32 final_color = (u32)((target & (0xff << 8 * 3)) * src_alpha_c + (src & (0xff << 8 * 3)) * src_alpha) // Red
+		| (u32)((target & (0xff << 8 * 2)) * src_alpha_c + (src & (0xff << 8 * 2)) * src_alpha) // Green
+		| (u32)((target & (0xff << 8 * 1)) * src_alpha_c + (src & (0xff << 8 * 1)) * src_alpha) // Blue
+		| 0xff; // Alpha
+	return final_color;
+}
+
+static const u32 xy_to_index(const u32 x, const u32 y, const u32 width)
+{
+	return width * y + x;
+}
+
+#endif // __SRC_UTILS_H__
