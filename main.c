35b35619e7a762999a89a08206e1ce310488503d
LuanFabricio <luanlimaleite@hotmail.com>
2024-10-22 00:12:06 -0300

    feat: Adding a grid draw on main.c

--- main.c
+++ main.c
@@ -4,9 +4,12 @@
 
 #include "src/types.h"
 #include "src/image.h"
+#include "src/utils.h"
 
-#define SCREEN_WITDH 720
-#define SCREEN_HEIGHT 640
+#define BLOCK_SIZE 64
+
+#define SCREEN_WITDH 16*BLOCK_SIZE
+#define SCREEN_HEIGHT 9*BLOCK_SIZE
 
 int main(void)
 {
@@ -43,9 +46,48 @@ int main(void)
 
 	image_append_image(&img, &img_test, (vec2u32_t){ .x = 4, .y = 4});
 
+	block_e blocks[SCREEN_WITDH*SCREEN_HEIGHT / BLOCK_SIZE] = {BLOCK_EMPTY};
+	scene_t scene = {
+		.width = SCREEN_WITDH / BLOCK_SIZE,
+		.height = SCREEN_HEIGHT / BLOCK_SIZE,
+		.blocks = blocks,
+	};
+	scene.blocks[0] = BLOCK_BRICKS;
+	scene.blocks[2] = BLOCK_BRICKS;
+
+	u32 scene_tex_buffer[SCREEN_WITDH*SCREEN_HEIGHT] = {0xff000000};
+	image_t scene_img = {
+		.width = SCREEN_WITDH,
+		.height = SCREEN_HEIGHT,
+		.pixel_buffer = scene_tex_buffer,
+	};
+	Texture2D scene_tex = LoadTextureFromImage((Image){
+		.mipmaps = 1,
+		.width = scene_img.width,
+		.height = scene_img.height,
+		.data = scene_img.pixel_buffer,
+		.format = PIXELFORMAT_UNCOMPRESSED_R8G8B8A8,
+	});
+
 	vec2f32_t speed = { 1.0f, 1.0f };
 	SetTargetFPS(60);
 	while(!WindowShouldClose()) {
+		image_clear(&scene_img, 0xff000000);
+		for (u32 yy = 0; yy < scene.height; yy++) {
+			for (u32 xx = 0; xx < scene.width; xx++) {
+				const u32 index = xy_to_index(xx, yy, scene.width);
+
+				switch (scene.blocks[index]) {
+					case BLOCK_BRICKS: {
+						image_draw_rectangle_color(&scene_img, xx*BLOCK_SIZE, yy*BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE, 0xff0000ff);
+					} break;
+					default:
+						break;
+				}
+			}
+		}
+		UpdateTexture(scene_tex, scene_img.pixel_buffer);
+
 		image_clear(&img, 0);
 		double time_sin, time_cos;
 		time_sin = sin(GetTime());
@@ -56,12 +98,14 @@ int main(void)
 			| (u32)(0xff * time_cos) << 8 * 1
 			| (u32)(0xff * GetFrameTime()) << 8 * 0;
 		image_draw_rectangle_color(&img, 0, 0, box1.w, box1.h, img_color);
+
 		UpdateTexture(tex, img.pixel_buffer);
 
 		BeginDrawing();
 			ClearBackground(BLACK);
 			// DrawRectangle(box1.x, box1.y, box1.w, box1.h, *(Color*)(&color));
 			DrawTexture(tex, box1.x, box1.y, WHITE);
+			DrawTexture(scene_tex, 0, 0, WHITE);
 		EndDrawing();
 
 		f32 delta_time = GetFrameTime();
