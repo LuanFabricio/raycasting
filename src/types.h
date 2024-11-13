c59cbc7d3c3c0542240b81f6eab9744b465b91d5
LuanFabricio <luanlimaleite@hotmail.com>
2024-11-12 23:28:45 -0300

    feat: Adding block color for the scene

--- src/types.h
+++ src/types.h
@@ -30,11 +30,35 @@ typedef struct {
 	u32 w, h;
 } box_t;
 
-typedef enum { BLOCK_EMPTY, BLOCK_BRICKS } block_e;
+typedef enum { BLOCK_EMPTY, BLOCK_BRICKS } block_type_e;
+
+// TODO: Define how to handle multiple render types
+// Example:
+// 	If a block have a sprite, use a ptr to that sprite.
+// 	If a block have a color, render the color.
+// 	Maybe use a a ptr approach
+// 	...
+// 	block_type_e block_type;
+// 	void *render_ptr;
+// 	...
+//	switch (block_type) {
+//		case BLOCK_BRICKS:
+//			render_color(render_ptr);
+//			break;
+//		case BLOCK_SPRITE:
+//			render_sprite(render_ptr);
+//			break;
+//		default:
+//			break;
+//	}
+typedef struct {
+	block_type_e block_type;
+	u32 color;
+} block_t;
 
 typedef struct {
 	u32 width, height;
-	block_e *blocks;
+	block_t *blocks;
 	vec2f32_t player_position;
 	f32 player_angle;
 } scene_t;
