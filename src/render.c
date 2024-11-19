0e81f68b4caec5cc133f2a9db9c272a7cb410750
LuanFabricio <luanlimaleite@hotmail.com>
2024-11-19 19:20:45 -0300

    fix: Reducing the operations on get_fov_plane

--- src/render.c
+++ src/render.c
@@ -10,21 +10,13 @@
 
 void get_fov_plane(const vec2f32_t pos, const f32 angle, const f32 scale, vec2f32_t out[2])
 {
-	vec2f32_t plane_middle = vec2f32_from_angle(BASE_ROTATION + angle);
-	vec2f32_scale(&plane_middle, scale, &plane_middle);
-	vec2f32_add(&plane_middle , &CAST_TYPE(vec2f32_t, pos), &plane_middle);
+	const f32 half_fov = FOV * 0.5f;
+	f32 left = scale / cos(half_fov);
+	out[0] = vec2f32_from_angle(angle - half_fov);
+	vec2f32_scale(&out[0], scale, &out[0]);
+	vec2f32_add(&pos, &out[0], &out[0]);
 
-	f32 left = tan(FOV * 0.5) * scale;
-
-	vec2f32_sub(&plane_middle, &CAST_TYPE(vec2f32_t, pos), &out[0]);
-	vec2f32_rot90(&out[0], &out[0]);
-	vec2f32_norm(&out[0], &out[0]);
-	vec2f32_scale(&out[0], left, &out[0]);
-	vec2f32_add(&plane_middle, &out[0], &out[0]);
-
-	vec2f32_sub(&plane_middle, &CAST_TYPE(vec2f32_t, pos), &out[1]);
-	vec2f32_rot90(&out[1], &out[1]);
-	vec2f32_norm(&out[1], &out[1]);
-	vec2f32_scale(&out[1], left, &out[1]);
-	vec2f32_sub(&plane_middle, &out[1], &out[1]);
+	out[1] = vec2f32_from_angle(angle + half_fov);
+	vec2f32_scale(&out[1], scale, &out[1]);
+	vec2f32_add(&pos, &out[1], &out[1]);
 }
