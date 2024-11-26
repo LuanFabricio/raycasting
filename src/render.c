cc4993f4cf12cf8e0f7579c268b5b80c6c3ebbdf
LuanFabricio <luanlimaleite@hotmail.com>
2024-11-26 19:52:21 -0300

    feat: Adds render_get_texture_x, render_get_texture_y and render_get_texture_color_index

--- src/render.c
+++ src/render.c
@@ -1,7 +1,6 @@
 #include <math.h>
 
 #include "types.h"
-#include "utils.h"
 #include "vec2f32.h"
 
 #include "defines.h"
@@ -19,4 +18,43 @@ void get_fov_plane(const vec2f32_t pos, const f32 angle, const f32 scale, vec2f3
 	out[1] = vec2f32_from_angle(angle + half_fov);
 	vec2f32_scale(&out[1], scale, &out[1]);
 	vec2f32_add(&pos, &out[1], &out[1]);
+}
+
+u32 render_get_texture_x(const vec2f32_t *hit_point, const u8 block_face)
+{
+	vec2f32_t grid_point = {0};
+	vec2f32_floor(hit_point, &grid_point);
+
+	vec2f32_t tex_point = {0};
+	vec2f32_sub(hit_point, &grid_point, &tex_point);
+
+	f32 u = 0;
+	switch (block_face) {
+		case 0:
+			u = 1 - tex_point.x;
+			break;
+		case 1:
+			u = 1 - tex_point.y;
+			break;
+		case 2:
+			u = tex_point.x;
+			break;
+		case 3:
+			u = tex_point.y;
+			break;
+		default:
+			break;
+	}
+
+	return floorf(u * TEXTURE_SIZE);
+}
+
+u32 render_get_texture_y(const u32 screen_y, const u32 render_y, const f32 text_height_prop)
+{
+	return floorf((screen_y - render_y) * text_height_prop);
+}
+
+u32 render_get_texture_color_index(const u32 tex_x, const u32 tex_y, const u32 tex_height)
+{
+	return tex_y * tex_height + tex_x;
 }
