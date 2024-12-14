29891979cbf58fbb65c6f027048058bf4595af12
LuanFabricio <luanlimaleite@hotmail.com>
2024-12-13 22:50:07 -0300

    feat: Adds scene_teleport_player

--- src/scene.c
+++ src/scene.c
@@ -1,4 +1,6 @@
 #include "types.h"
+#include "vec2f32.h"
+#include "utils.h"
 
 #include "scene.h"
 
@@ -16,4 +18,37 @@ void scene_get_block_points(u32 x, u32 y, float scale, vec2f32_t *out)
 	// (x, y+1)
 	out[3].x = x	 * scale;
 	out[3].y = (y+1) * scale;
+}
+
+void scene_teleport_player(scene_t *scene, const block_t *block, const f32 delta_time)
+{
+		printf("Hit portal %u\n", block->portal);
+		portal_t *portal = (block->portal == PORTAL_1) ? &scene->portal1 : &scene->portal2;
+		const f32 angle = update_speed_direction(portal);
+		vec2f32_t speed_mul = vec2f32_from_angle(angle);
+		vec2f32_norm(&speed_mul, &speed_mul);
+
+		switch (block->portal) {
+			case PORTAL_1: {
+				printf("Moving to (%u, %u)\n", scene->portal2.position.x, scene->portal2.position.y);
+				scene->player_position.x = scene->portal2.position.x;
+				scene->player_position.y = scene->portal2.position.y;
+			} break;
+			case PORTAL_2: {
+				printf("Moving to (%u, %u)\n", scene->portal1.position.x, scene->portal1.position.y);
+				scene->player_position.x = scene->portal1.position.x;
+				scene->player_position.y = scene->portal1.position.y;
+			} break;
+
+			default:
+				break;
+
+		}
+
+		printf("Speed mul: %f, %f\n", speed_mul.x, speed_mul.y);
+		// TODO: Rotate player angle when teleports
+		scene->player_angle = angle;
+		scene->player_position.x += 1.2f * speed_mul.x + (1.0f - speed_mul.x) * 0.5;
+		scene->player_position.y += 1.2f * speed_mul.y + (1.0f - speed_mul.y) * 0.5;
+		printf("At %f, %f\n", scene->player_position.x, scene->player_position.y);
 }
--- src/scene.h
+++ src/scene.h
@@ -4,5 +4,6 @@
 #include "types.h"
 
 void scene_get_block_points(u32 x, u32 y, float scale, vec2f32_t *out);
+void scene_teleport_player(scene_t *scene, const block_t *block, const f32 delta_time);
 
 #endif // __SRC_SCENE_H__
