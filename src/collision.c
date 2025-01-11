f1a6fbc5d384777464eb5024b59e5a809da91754
LuanFabricio <luanlimaleite@hotmail.com>
2025-01-11 15:07:54 -0300

    fix: Moves to a different approach to make the entity wider by the player PoV

--- src/collision.c
+++ src/collision.c
@@ -189,18 +189,24 @@ bool collision_hit_an_entity(const scene_t *scene, const vec2f32_t p1, const vec
 	f32 dist = FLT_MAX;
 	vec2f32_t current_hit = {0};
 
-	const vec2f32_t entity_ray = vec2f32_from_angle(scene->player.angle + PI);
 	for (u32 i = 0; i < scene->entities.lenght; i++) {
 		const vec2f32_t pos = scene->entities.data[i].position;
+		vec2f32_t entity_player_dir = {
+			.x = p1.x - pos.x,
+			.y = p1.y - pos.y,
+		};
+		vec2f32_norm(&entity_player_dir, &entity_player_dir);
+
 		const vec2f32_t pos1 = {
-			.x = pos.x + entity_ray.y * 0.5f,
-			.y = pos.y - entity_ray.x * 0.5f,
+			.x = pos.x - 0.5 * -entity_player_dir.y,
+			.y = pos.y - 0.5 * entity_player_dir.x,
 		};
 
 		const vec2f32_t pos2 = {
-			.x = pos.x - entity_ray.y * 0.5f,
-			.y = pos.y + entity_ray.x * 0.5f,
+			.x = pos.x + 0.5 * -entity_player_dir.y,
+			.y = pos.y + 0.5 * entity_player_dir.x,
 		};
+
 		if (collision_intersects(p1, p2, pos1, pos2, &current_hit)) {
 			f32 current_dist = vec2f32_distance(&p1, &current_hit);
 			if (current_dist < dist) {
